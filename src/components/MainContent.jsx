a2f8529e3deb3a1379e31537ae700a09ebf014bf
prashantghadiali <er.prashantghadiali@gmail.com>
2023-08-04 12:39:22 +0530

    initial commit

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,24 @@
+const MainContent = () => {
+    return (
+        <>
+        <div className="forabsimg">
+            <img className="intro-cube-1" src="https://s3-alpha-sig.figma.com/img/6c61/a7be/f4536bf35ee6fc5748edc2ca835f1604?Expires=1691971200&Signature=pQgu0Wxyt4FCyWfWSO1WB41Lm0HNcIXZ8tr1SRMWe0FA8BRYzFoXcMRYPPaQWXeT~w0niS1xRhwdin~zBj2q6AUATqUrE6ozMf-cjUQYGFlW9tTPpLRCynEoo1y9Jdz3462z-Mma4LNC1i6SLT4-I-RBq7J84ZLMofEDqH~XviMz1rbpop9SlXLok5DebGxgOGaf5zi4iNl8hDwrNXw71I19sht~SrNngexmrDb9QpvN-rehFF5hYij~2H7992xaU14oR7U8DpAt89F0aWUSNiv1kgPlAlTXhcqX6JFsqNUQHQgymE9912lFEOfwSGglPCU4OksrhdCpcH0ZRahVyw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" srcset="" />
+            <img className="intro-stairs-1" src="https://s3-alpha-sig.figma.com/img/6d5e/17ae/5ee69c97f9ba57a878c43da699dda069?Expires=1691971200&Signature=mp7nXpNUs0BdhPRfCuxO9fVCcU77OVBZtqonb5mRGuycqPgKyVsvQ9NaMLr25grBDG-yxBn9FEATIp9Vi70QS03pM9vkvGHWYsbIsweOHrIVbet2CQce2dCkPqkLUCna8o5Dc2yDEwoziWJbtxG6U70xdlpCTDjMDOk7JGKWQtSSkVWTfVIX~XcfnvlO7t~bWXbPOUHuQfzWHeWvhCBsn6VFf~4A-Ek0ce5L8pipXAtqLa-9NujFUvGI6QcghuCpdGoX0AV9yQKjH0i9ZX19nsrkPuQGFClTQIzFv2awARLHrK3x1CmvMZLBY~kllPS9HC2MsJPzNmijRPq~~n4kmA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" srcset="" />
+            <img className="introcubes-1" src="https://s3-alpha-sig.figma.com/img/dc74/e512/17aa9b0da75a6fa251f15bdfbdc53e01?Expires=1691971200&Signature=X8nuYdU3vnZLtkVvDH9UEfHR~vezpAVx-PLW1-yD~Oy7ir5QdqK0YTN4KHh3smVu46~Bv72p7bBHumjE6U97o3PJrCLCo46~lU0yzJEnEAayB9XtUT6TJf0ry7V5vvpTy5pOxKkpPoqgbTQMRENxZvLhuhkCnYOBS1Eed86p6C7arvNYpXogwBnPj8kyH9NZkGG2EDBsWSoaKZnDd4If4rw9fq0bnXNvs6j12rlpeypl2R4vnTNSM0DoJeA~L4c1dXIBrhA92giyKT~BYdeuieFx4ERLztJiE~TevvPXMZmFLVhL59~OkzHnTHO6h0PxyNcsLoj6EGRCHpAAruFCOQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" srcset="" />
+            <div className="crop-container">
+                <img className="intro-pillar" src="https://s3-alpha-sig.figma.com/img/71a8/40d9/b96ead2b95903e65967882722b31191c?Expires=1691971200&Signature=XTtGg8LZovlI-KqS8mSY6rPamzelKvvEkQgHPjaKKy3xqxwQPw3YlpfDvGmz0LHBTtbdOW9hjPi5L-lhuYrOxZNwJWcozduuNAu5ukpF2Kjsg8w8VWerwP1iBY-VnnFr0-iwqFiwKpN19jZB~X0udVTjoKu0Mo3dt-dvQ32Kl9uUP62g7ivK20Sl96X-eM4~evDTuvzHwknLmCN6Lxn1zLfVl608M4e-DxADSccq9tioFddO-Ed1tRpo~VuzqZaHbaJJRqjG2bCd1TxzxzdVZlrN0nLDEik-vD384T-DZhSr-bvlNv6WsCVCsTTCqByMM4SNr3R4RmLsUWOY4Lt5vg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" srcset="" />
+            </div>
+            
+        </div>
+        <div className="Hero">
+            <div class="selected">
+                <p>CASH</p>
+                <img className="phone" src="https://s3-alpha-sig.figma.com/img/8d46/5b02/4c6355058dc37af6efb4a5847d5e81cc?Expires=1691971200&Signature=j5SHOA4lwwMGR7xSxQqJoR29XEvGMuXmLf~ufwM5j4161bvSUiUPQD2vzXxwqAxeWI5bzU8SOJAf-TWXYoro08GtgELsCA6ye65hl6xtoXqK6aO3TOlEpXwT8UEMrNZd6xhnOmfVWGb0OZK9cx8npCJfGFovHFXHdSYfN9B9Pk0iPxBPvL1bM1jBusRWrITrRGnnqbyZBpJ4o66VgbISi50YC2-lG3M8bU-RmQjn3PM01SgO7WlhqbV0Rpxga3CfPRorL8VgtUxq2dOHoAaPDt0YA5G8XAUocZKHoJcQqzToKczFJ~K5z7e7cKAEQmbU-9CmK1IzCW7ts6aGxyT~Ng__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" srcset="" />
+                <p id="apptext">APP</p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default MainContent
